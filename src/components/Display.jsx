5206acb35f02c312bfc026fa26aa74bdc11c53af
cristiane pereira da silva <cristiane.pereira901113@gmail.com>
2021-07-25 21:56:17 -0300

    Calculadora React

---
+++ src/components/Display.jsx
@@ -0,0 +1,8 @@
+import react from 'react'
+import './Display.css'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>
+ <div className="display">
+     {props.value}
+ </div>
